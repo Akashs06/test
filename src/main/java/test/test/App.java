a9c7428981b13af61ff1e13422ce788be64a2a3a
Akashs06 <akashskrish@gmail.com>
2023-02-06 17:08:03 +0530

    delete all testing

--- src/main/java/test/test/App.java
+++ src/main/java/test/test/App.java
@@ -9,33 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println("merge");
-        System.out.println("merge");
-        System.out.println("merge");
-        System.out.println("merge");
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println("merge");
-        System.out.println("merge");
-        System.out.println("merge");
-        System.out.println("merge");
-        
+
     }
 }
